a574afed59cb696baeaa80e78fbf243d2470bd93
EdwardSoto21 <igrofob2001@gmail.com>
2024-01-27 15:24:11 +0100

    test2

---
+++ test.py
@@ -0,0 +1,19 @@
+import json
+
+def zapisz_fakture():
+    kwota = float(input("Podaj kwotę faktury: "))
+    data_faktury = input("Podaj datę faktury (w formacie YYYY-MM-DD): ")
+    waluta = input("Podaj walutę: ")
+
+    faktura = {
+        "Kwota": kwota,
+        "Data_faktury": data_faktury,
+        "Waluta": waluta
+    }
+
+    with open("faktury.txt", "a") as plik:
+        json.dump(faktura, plik)
+        plik.write('\n')
+
+# Wywołanie funkcji
+zapisz_fakture()
